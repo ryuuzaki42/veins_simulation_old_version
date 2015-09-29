fae570a26f1ae8982d6ec5ae58902885d5c9620b
João Batista <joao42lbatista@gmail.com>
2015-09-29 09:32:07 -0300

    Tudo Certo! 09/29/15 09:32

--- run_sumo.sh
+++ run_sumo.sh
@@ -1,10 +1,10 @@
 cd /media/sda4/prog/veins-3.0/
 echo -e "Deseja executar sumo com interface gráfica:\n (y)es - sumo-gui ou (n)o sumo"
 read resposta
-if [ $resposta = y ] 
-	then
-	echo -e "Running sumo-gui\n"
-	python sumo-launchd.py -vv -c sumo-gui
+if [ $resposta = y ]
+   then
+   echo -e "Running sumo-gui\n"
+   python sumo-launchd.py -vv -c sumo-gui
 fi
 if [ $resposta = n ]
   then
