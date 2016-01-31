66be02b53c176248f713d4193c6a49e89a2d1841
João Batista <joao42lbatista@gmail.com>
2016-01-31 10:45:11 -0200

    netgenerate: added default.speed 85 randomTrips.py: added -s (seed) 1, for the same routes everytime.
    Adde test "is the same routes?"
    cat test.rou.xml | grep -v generated | md5sum
    f649688c8178367ce3f5dade28de4f3b  -

--- projects/vehDist/routes/comandos_read.r
+++ projects/vehDist/routes/comandos_read.r
@@ -3,11 +3,14 @@ http://sumo.dlr.de/wiki/Tools/Trip
 python /media/sda4/prog/sumo-0.21.0/tools/trip/randomTrips.py --help
 http://sumo.dlr.de/wiki/FAQ#How_do_I_generate_random_routes.3F
 
-g++ -std=c++0x -o script_conversor script_conversor.cc ^C
-
 ## Generate the Grid
-netgenerate  -g --grid.number=5 --grid.length=250 --default.lanenumber=1 --no-turnarounds -o test.net.xml
-# Change --default.lanenumber to 1 and added --no-turnarounds
+netgenerate  -g --grid.number=5 --grid.length=250 --default.lanenumber=1 --no-turnarounds --default.speed 85 -o test.net.xml
+# Change --default.lanenumber to 1; added --no-turnarounds and the default.speed to 85
 
 ## Generate the trips and the routes (with various distance)
-python /media/sda4/prog/sumo-0.21.0/tools/trip/randomTrips.py -n test.net.xml --min-distance=100000 -b 0 -e 30000 -i 200 -r test.rou.xml
+python /media/sda4/prog/sumo-0.21.0/tools/trip/randomTrips.py -n test.net.xml --min-distance=100000 -b 0 -e 30000 -i 200 -s 1 -r test.rou.xml
+# Change added -s (seed) 1, for the same routes everytime.
+
+To test "is the same routes?"
+cat test.rou.xml | grep -v generated | md5sum
+f649688c8178367ce3f5dade28de4f3b  -
