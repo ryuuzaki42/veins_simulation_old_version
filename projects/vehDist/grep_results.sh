61ada8b0e4480da5faf9ea66cd30b429c8334c79
João Batista <joao42lbatista@gmail.com>
2016-02-02 23:42:39 -0200

    start script grep_results.sh

---
+++ projects/vehDist/grep_results.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Autor= João Batista Ribeiro
+# Bugs, Agradecimentos, Criticas "construtiva"
+# Mande me um e-mail. Ficarei Grato!
+# e-mail: joao42lbatista@gmail.com
+#
+# Este programa é um software livre; você pode redistribui-lo e/ou
+# modifica-lo dentro dos termos da Licença Pública Geral GNU como
+# publicada pela Fundação do Software Livre (FSF); na versão 2 da
+# Licença, ou (na sua opinião) qualquer versão.
+#
+# Este programa é distribuído na esperança que possa ser útil,
+# mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a
+# qualquer MERCADO ou APLICAÇÃO EM PARTICULAR.
+#
+# Veja a Licença Pública Geral GNU para maiores detalhes.
+# Você deve ter recebido uma cópia da Licença Pública Geral GNU
+# junto com este programa, se não, escreva para a Fundação do Software
+#
+# Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Script: Scrpit to collect the simulation result in one place
+#
+# Última atualização: 02/02/2016
+#
+echo "Scrpit to collect the simulation result in one place"
+cat VehMessagesgenerated.r | egrep "Execution number"" 
