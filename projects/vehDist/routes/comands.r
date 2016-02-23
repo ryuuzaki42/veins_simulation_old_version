7ebf870e2271451de6f018ab3831cd8c312255c5
João Batista <joao42lbatista@gmail.com>
2016-02-23 14:40:07 -0300

    (max) speed to 85

--- projects/vehDist/routes/comands.r
+++ projects/vehDist/routes/comands.r
@@ -4,8 +4,8 @@ python /media/sda4/prog/sumo-0.21.0/tools/trip/randomTrips.py --help
 http://sumo.dlr.de/wiki/FAQ#How_do_I_generate_random_routes.3F
 
 ## Generate the Grid
-netgenerate  -g --grid.number=5 --grid.length=250 --default.lanenumber=1 --no-turnarounds --default.speed 15 -o test.net.xml
-# Change --default.lanenumber to 1; added --no-turnarounds and the default.speed to 15
+netgenerate -g --grid.number=5 --grid.length=250 --default.lanenumber=1 --no-turnarounds --default.speed 85 -o test.net.xml
+# Change --default.lanenumber to 1; added --no-turnarounds and the (max) speed to 85
 
 ## Generate the trips and the routes (with various distance)
 python /media/sda4/prog/sumo-0.21.0/tools/trip/randomTrips.py -n test.net.xml --min-distance=100000 -b 0 -e 30000 -i 200 -s 1 -r test.rou.xml
