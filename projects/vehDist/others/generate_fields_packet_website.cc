5b4aad33934e4f9ced82e45e1062ac3c47321464
João Batista <joao42lbatista@gmail.com>
2016-03-29 15:09:23 -0300

    Added generate_fields_packet_website.cc to generate the fields-tex-size to image to packet in the website http://interactive.blockdiag.com/packetdiag/

---
+++ projects/vehDist/others/generate_fields_packet_website.cc
@@ -0,0 +1,54 @@
+//#########################################################################################
+//                                         To compile:                                    #
+// g++ -std=c++0x -o generate_fields_packet_website.out generate_fields_packet_website.cc #
+//#########################################################################################
+
+// Scritp to generate the text for generate the packat-image in 
+// the website http://interactive.blockdiag.com/packetdiag/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+   int size, countFields, countLimit, fieldsValue;
+   string nome;
+
+   cout << "Digite a quantidade de campos: ";
+   cin >> countLimit;
+   countFields = 1;
+   fieldsValue = 0;
+
+   std::ofstream myfile;
+   myfile.open("diagram.txt");
+   myfile << "packetdiag {" << endl;
+   myfile << "    #colwidth = 16" << endl; 
+   myfile << "    #node_height = 80" << endl;
+   myfile << "    #node_width = 20" << endl << endl;
+
+   while (countFields <= countLimit){
+      while (cin.get() != '\n') {
+         continue;
+      }
+
+      cout << endl;
+
+      cout << "Nome_" << countFields << ": ";
+      getline(cin, nome);
+
+      cout << "Tamanho: ";
+      cin >> size;
+
+      myfile << "    " << fieldsValue << "-" << (fieldsValue + size - 1) << ": " << nome << endl;
+      fieldsValue += size;
+
+      countFields++;
+   }
+
+   myfile << "}";
+   cout << endl << "\tDiagram saved in the file diagram.txt" << endl << endl;
+   myfile.close();
+
+   return 0;
+}
