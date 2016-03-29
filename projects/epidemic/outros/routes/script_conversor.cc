31b99922185ccc630dc12f5a6bcace473ff18460
João Batista <joao42lbatista@gmail.com>
2016-03-29 18:15:17 -0300

    Update script_conversor g++ -std=c++0x and SUMO 0.25.0

--- projects/epidemic/outros/routes/script_conversor.cc
+++ projects/epidemic/outros/routes/script_conversor.cc
@@ -1,24 +1,14 @@
-#include <sstream> 
-#include <string>
+//#############################################################
+//                     To compile:                            #
+// g++ -std=c++0x -o script_conversor.out script_conversor.cc # 
+//#############################################################
+
 #include <iostream>
 #include <fstream>
 
 using namespace std;
 
-template <typename T>
-std::string to_string(T value){
-    //create an output string stream
-    std::ostringstream os ;
-
-    //throw the value into the string stream
-    os << value ;
-
-    //convert the string stream into a string and return
-    return os.str() ;
-}
-
-int main(){
-
+int main() {
     freopen("test.rou.xml","r",stdin); //Arquivo de entrada gerado com script randomTrips.py
 
     string line;
@@ -30,29 +20,27 @@ int main(){
     //Escrita da definição do tipo de veículo no arquivo de saída
     output << "<routes>\n    <vType id=\"vtype0\" accel=\"2.6\" decel=\"4.5\" sigma=\"0.5\" length=\"2.5\" minGap=\"2.5\" maxSpeed=\"14\" color=\"1,1,0\"/>\n\n";
 
-
     int count=0;
 
-    while (getline(cin,line)){
-        if (line.compare(0,15,"        <route ") == 0){ //Edita cada linha do arquivo de entrada que representa rotas
+    while (getline(cin,line)) {
+        if (line.compare(0,15,"        <route ") == 0) { //Edita cada linha do arquivo de entrada que representa rotas
             string to = "    <route id=\"";
             to +="route" + to_string(count++);
             to +="\"";
             to += line.substr(14) + "\n";
             // 23 no início [        <route edges="] + 3 no final ["/>] => 26; 1 tem 9 [1/2to1/1 ], logo 1 km => 4 *9 => 36; 10 km => 360. 360 + 26 = 386
             // logo to.size tem que ter pelo menos 386.
-            if (to.size() > 400 ){
+            if (to.size() > 400 ) {
                 output << to; //Escrita da rota no arquivo de saída
                 //cout << endl << "to.size: "<< to.size() << endl;
                 cout << to; //Imprimir rotas geradas no terminal
-            }else{
+            } else {
                 cout << endl << "to.size, menor que 400: "<< to.size() << endl;
                 count--;
             }
         }
-  }
+    }
 
     output << "</routes>"; //Finalização do arquivo de saída
-
     return 0;
 }
