49360b2f4c0d20fd3b13186c364ed882f0dddc0b
João Batista <joao42lbatista@gmail.com>
2016-03-29 18:14:00 -0300

    Update SUMO 0.25.0

--- projects/vehDist/others/comands.r
+++ projects/vehDist/others/comands.r
@@ -1,29 +1,29 @@
 ## randomTrips
     http://sumo.dlr.de/wiki/Tools/Trip
-    python /media/sda4/prog/sumo-0.21.0/tools/trip/randomTrips.py --help
+    python /media/sda4/prog/sumo-0.25.0/tools/randomTrips.py --help
     http://sumo.dlr.de/wiki/FAQ#How_do_I_generate_random_routes.3F
 
 ## Generate the Grid
     netgenerate -g --grid.number=5 --grid.length=250 --default.lanenumber=1 --default.speed 85 -o test.net.xml
-        #netgenerate -g --grid.number=5 --grid.length=250 --default.lanenumber=1 --no-turnarounds --default.speed 85 -o test.net.xml
+        # netgenerate -g --grid.number=5 --grid.length=250 --default.lanenumber=1 --no-turnarounds --default.speed 85 -o test.net.xml
         # Change --default.lanenumber to 1; added --no-turnarounds and the (max) speed to 85
 
 ## Generate the trips and the routes (with various distance)
-    python /media/sda4/prog/sumo-0.21.0/tools/trip/randomTrips.py -n test.net.xml --min-distance=100000 -b 0 -e 30000 -i 200 -s 1 -r test.rou.xml
+    python /media/sda4/prog/sumo-0.25.0/tools/randomTrips.py -n test.net.xml --min-distance=100000 -b 0 -e 30000 -i 200 -s 1 -r test.rou.xml
         # Change added -s (seed) 1, for the same routes every time.
 
 ## To test "is the same routes?"
     cat test.rou.xml | grep -v generated | md5sum
 
-    f649688c8178367ce3f5dade28de4f3b  - with --no-turnarounds
-    026a7f8c52c9f5c6e613ccd82e00d831  - without --no-turnarounds
+    ? - with --no-turnarounds
+    2c45d48ea2a3fba1667a1bbe7c8a7827 - without --no-turnarounds
 
 ## Test same start position
     cat [folder]/vehicle_position_initialize.r | grep -v number  | md5sum
 
 ## Change maxSpeed
-    sed -i 's/maxSpeed=".."/maxSpeed="15"/g' test_end.rou.xml
-    sed -i 's/maxSpeed=".."/maxSpeed="25"/g' test_end.rou.xml
+    sed -i 's/maxSpeed=.* color/maxSpeed=\"15\" color/g' test_end.rou.xml
+    sed -i 's/maxSpeed=.* color/maxSpeed=\"25\" color/g' test_end.rou.xml
 
 ## See iterations from ini file
     #cat General-*.sca | grep "attr iterationvars2"
