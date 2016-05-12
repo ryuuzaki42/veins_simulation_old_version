c0aae1be1c78e60fa57604d697c3e96c9484554b
João Batista <joao42lbatista@gmail.com>
2016-05-12 16:14:16 -0300

    Remove with _end.r, now only .r

---
+++ projects/vehDist/distributionCategory.r
@@ -0,0 +1,49 @@
+## Distribuição dos veículos pelos segmentos de ruas
+cP = Veículos de Passeio.   cT = Táxi
+
+Nome do segmento de rua                  Count cP    Count cT    %cP    %cT    cT + cP
+
+Street_Segment_01: 0/0to0/1 | 0/1to0/0    cT + cP: 25     cP: 20     cT: 5     %cP: 80    %cT: 20
+Street_Segment_02: 0/1to0/2 | 0/2to0/1    cT + cP: 64     cP: 52     cT: 12    %cP: 81.25    %cT: 18.75
+Street_Segment_03: 0/1to1/1 | 1/1to0/1    cT + cP: 60     cP: 41     cT: 19    %cP: 68.33    %cT: 31.67
+Street_Segment_04: 0/2to1/2 | 1/2to0/2    cT + cP: 24     cP: 18     cT: 6     %cP: 75    %cT: 25
+Street_Segment_05: 0/3to0/2 | 0/2to0/3    cT + cP: 63     cP: 50     cT: 13    %cP: 79.37    %cT: 20.63
+Street_Segment_06: 0/3to0/4 | 0/4to0/3    cT + cP: 28     cP: 23     cT: 5     %cP: 82.14    %cT: 17.86
+Street_Segment_07: 0/4to1/4 | 1/4to0/4    cT + cP: 17     cP: 11     cT: 6     %cP: 64.71    %cT: 35.29
+Street_Segment_08: 1/0to0/0 | 0/0to1/0    cT + cP: 21     cP: 16     cT: 5     %cP: 76.19    %cT: 23.81
+Street_Segment_09: 1/1to1/0 | 1/0to1/1    cT + cP: 72     cP: 61     cT: 11    %cP: 84.72    %cT: 15.28
+Street_Segment_10: 1/1to1/2 | 1/2to1/1    cT + cP: 76     cP: 58     cT: 18    %cP: 76.32    %cT: 23.68
+Street_Segment_11: 1/2to1/3 | 1/3to1/2    cT + cP: 97     cP: 72     cT: 25    %cP: 74.23    %cT: 25.77
+Street_Segment_12: 1/2to2/2 | 2/2to1/2    cT + cP: 39     cP: 27     cT: 12    %cP: 69.23    %cT: 30.77
+Street_Segment_13: 1/3to0/3 | 0/3to1/3    cT + cP: 51     cP: 38     cT: 13    %cP: 74.51    %cT: 25.49
+Street_Segment_14: 1/4to1/3 | 1/3to1/4    cT + cP: 75     cP: 55     cT: 20    %cP: 73.33    %cT: 26.67
+Street_Segment_15: 2/0to1/0 | 1/0to2/0    cT + cP: 45     cP: 39     cT: 6     %cP: 86.67    %cT: 13.33
+Street_Segment_16: 2/1to1/1 | 1/1to2/1    cT + cP: 59     cP: 41     cT: 18    %cP: 69.49    %cT: 30.51
+Street_Segment_17: 2/1to2/0 | 2/0to2/1    cT + cP: 29     cP: 21     cT: 8     %cP: 72.41    %cT: 27.59
+Street_Segment_18: 2/1to2/2 | 2/2to2/1    cT + cP: 53     cP: 38     cT: 15    %cP: 71.7    %cT: 28.3
+Street_Segment_19: 2/2to2/3 | 2/3to2/2    cT + cP: 45     cP: 33     cT: 12    %cP: 73.33    %cT: 26.67
+Street_Segment_20: 2/2to3/2 | 3/2to2/2    cT + cP: 43     cP: 30     cT: 13    %cP: 69.77    %cT: 30.23
+Street_Segment_21: 2/3to1/3 | 1/3to2/3    cT + cP: 74     cP: 61     cT: 13    %cP: 82.43    %cT: 17.57
+Street_Segment_22: 2/3to3/3 | 3/3to2/3    cT + cP: 60     cP: 41     cT: 19    %cP: 68.33    %cT: 31.67
+Street_Segment_23: 2/4to1/4 | 1/4to2/4    cT + cP: 45     cP: 32     cT: 13    %cP: 71.11    %cT: 28.89
+Street_Segment_24: 2/4to2/3 | 2/3to2/4    cT + cP: 34     cP: 28     cT: 6     %cP: 82.35    %cT: 17.65
+Street_Segment_25: 3/0to2/0 | 2/0to3/0    cT + cP: 35     cP: 29     cT: 6     %cP: 82.86    %cT: 17.14
+Street_Segment_26: 3/0to3/1 | 3/1to3/0    cT + cP: 27     cP: 22     cT: 5     %cP: 81.48    %cT: 18.52
+Street_Segment_27: 3/1to2/1 | 2/1to3/1    cT + cP: 50     cP: 34     cT: 16    %cP: 68    %cT: 32
+Street_Segment_28: 3/2to3/1 | 3/1to3/2    cT + cP: 27     cP: 19     cT: 8     %cP: 70.37    %cT: 29.63
+Street_Segment_29: 3/2to3/3 | 3/3to3/2    cT + cP: 30     cP: 22     cT: 8     %cP: 73.33    %cT: 26.67
+Street_Segment_30: 3/2to4/2 | 4/2to3/2    cT + cP: 34     cP: 25     cT: 9     %cP: 73.53    %cT: 26.47
+Street_Segment_31: 3/3to3/4 | 3/4to3/3    cT + cP: 13     cP: 7      cT: 6     %cP: 53.85    %cT: 46.15
+Street_Segment_32: 3/3to4/3 | 4/3to3/3    cT + cP: 32     cP: 25     cT: 7     %cP: 78.12    %cT: 21.88
+Street_Segment_33: 3/4to2/4 | 2/4to3/4    cT + cP: 43     cP: 38     cT: 5     %cP: 88.37    %cT: 11.63
+Street_Segment_34: 4/0to3/0 | 3/0to4/0    cT + cP: 19     cP: 14     cT: 5     %cP: 73.68    %cT: 26.32
+Street_Segment_35: 4/0to4/1 | 4/1to4/0    cT + cP: 14     cP: 8      cT: 6     %cP: 57.14    %cT: 42.86
+Street_Segment_36: 4/1to3/1 | 3/1to4/1    cT + cP: 29     cP: 21     cT: 8     %cP: 72.41    %cT: 27.59
+Street_Segment_37: 4/2to4/1 | 4/1to4/2    cT + cP: 16     cP: 11     cT: 5     %cP: 68.75    %cT: 31.25
+Street_Segment_38: 4/3to4/2 | 4/2to4/3    cT + cP: 14     cP: 11     cT: 3     %cP: 78.57    %cT: 21.43
+Street_Segment_39: 4/4to3/4 | 3/4to4/4    cT + cP: 19     cP: 16     cT: 3     %cP: 84.21    %cT: 15.79
+Street_Segment_40: 4/4to4/3 | 4/3to4/4    cT + cP: 19     cP: 12     cT: 7     %cP: 63.16    %cT: 36.84
+
+CountTotal: 1620 countP: 1220    countT: 400
+
+Porcentagem geral, %%GcP: 0.7531    %%GcT: 0.2469
