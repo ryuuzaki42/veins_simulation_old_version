9fe00f343193ba58a0425410de82de1530251715
João Batista <joao42lbatista@gmail.com>
2016-05-12 16:31:21 -0300

    Corrected error in generate (copy) the route

--- projects/vehDist/others/2_path_routes.cc
+++ projects/vehDist/others/2_path_routes.cc
@@ -17,6 +17,7 @@ int main(){
     output << "<routes>\n" << endl; // Escrita da definição do tipo de veículo no arquivo de saída
 
     bool parte1 = true;
+    bool flagContinue;
     string routePart1, routePart2, to, toTmp, line;
     int count, countVehicleCagegoryA, p1, p2, dist, countVehicleRoutes, routeComp;
     count = 1;
@@ -28,54 +29,60 @@ int main(){
 
     cout << endl << "Por favor espere, gerando rotas..." << endl << endl;
     while (getline(cin,line) && count <= countVehicleRoutes) { // count < 50 to create 50 rotas
-        if (line.compare(0,15,"        <route ") == 0) { // Edita cada linha do arquivo de entrada que representa rotas
+        if (line.compare(0, 15, "        <route ") == 0) { // Edita cada linha do arquivo de entrada que representa rotas
             to = "    <route id=\"";
             to += "route" + to_string(count);
             to += "\"";
             toTmp = to;
             to += line.substr(14) + "\n";
 
-            if (parte1) {
-                p1 = 22;
-                p2 = 85;
-                while ((p2 + 12) < to.size()) {
-                    dist = 8;
-                    while ((p2 + 12) < to.size()) {
-                        routePart1 = line.substr(p1,8); // Pega o primeiro ponto (ponto de partida)
-                        routePart2 = line.substr(p2,8); // Pega o oitavo ponto (ponto de partida)
-                        if (strcmp(routePart1.c_str(), routePart2.c_str()) == 0) {
+            //if (to.size() > routeComp) {
+                cout << "to.size: " << to.size() << endl;
+                if (parte1) {
+                    p1 = 22;
+                    p2 = 85;
+                    flagContinue = true;
+                    while ((p2 + 12) < to.size() && flagContinue) {
+                        dist = 8;
+                        while ((p2 + 12) < to.size() && flagContinue) {
+                            routePart1 = line.substr(p1,8); // Pega o primeiro ponto (ponto de partida)
+                            routePart2 = line.substr(p2,8); // Pega o oitavo ponto (ponto de partida)
+                            if (strcmp(routePart1.c_str(), routePart2.c_str()) == 0) {
 //     <route edges="0/3to1/3 1/3to1/2 1/2to1/1 1/1to2/1 2/1to3/1 3/1to4/1 4/1to4/0 4/0to3/0 3/0to2/0 2/0to2/1 2/1to2/2...
-                            if (dist == 9) { // dist == 9 para ter 8 ponto na rota
-                                cout << " route: " << count << " " << line.substr(p1,(p2 - p1 + 8)) << " dist: " << dist << " p1: " << p1 << " p2: "<< p2 << endl;
-                                toTmp += " edges=\"";
-                                while (toTmp.size() < routeComp) {
-                                    toTmp += line.substr(p1,(p2 - p1));
-                                }
-                                toTmp += "\"/>\n";
-                                output << toTmp;
-                                p2 = toTmp.size();
+                                if (dist == 9) { // dist == 9 para ter 8 ponto na rota
+                                    //cout << " route: " << count << " " << line.substr(p1,(p2 - p1 + 8)) << " dist: " << dist << " p1: " << p1 << " p2: "<< p2 << endl;
+                                    toTmp += " edges=\"";
+                                    cout << "routeID:" << count << "    toTmp.size(): " <<  toTmp.size() << endl;
+                                    while (toTmp.size() < routeComp) {
+                                        toTmp += line.substr(p1,(p2 - p1));
+                                    }
+                                    toTmp += "\"/>\n";
+                                    output << toTmp;
+                                    flagContinue = false;
+                                    p2 = toTmp.size();
 
-                                count++;
-                                if (count > countVehicleCagegoryA){
-                                    parte1 = false;
+                                    count++;
+                                    if (count > countVehicleCagegoryA){
+                                        parte1 = false;
+                                    }
                                 }
                             }
+                            p2 += 9;
+                            dist++;
                         }
-                        p2 += 9;
-                        dist++;
+                        p1 += 9;
+                        p2 = p1 + 9;
                     }
-                    p1 += 9;
-                    p2 = p1 + 9;
-                }
-            } else {
-                if (to.size() > routeComp){
-                    cout << " route: " << count << " \"random\"" << endl;
-                    output << to; // Escrita da rota no arquivo de saída
-                    count++;
                 } else {
-                    //cout << endl << "to.size, menor que" << routeComp << " : " << to.size() << endl;
+                    if (to.size() > routeComp){
+                        cout << " route: " << count << " \"random\"" << endl;
+                        output << to; // Escrita da rota no arquivo de saída
+                        count++;
+                    } else {
+                        //cout << endl << "to.size, menor que" << routeComp << " : " << to.size() << endl;
+                    }
                 }
-            }
+            //}
         }
     }
 
