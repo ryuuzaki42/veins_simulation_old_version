9049b1b1e6a0017a1075f7a9dfd036f296b4d3ca
João Batista <joao42lbatista@gmail.com>
2016-05-31 15:30:15 -0300

    Added count grid infos

--- projects/vehDist/others/comands.r
+++ projects/vehDist/others/comands.r
@@ -3,6 +3,8 @@
     python /media/sda4/prog/sumo-0.25.0/tools/randomTrips.py --help
     http://sumo.dlr.de/wiki/FAQ#How_do_I_generate_random_routes.3F
 
+## km^2, grid: (1,5) (2,9) (3,13) (4,17) (5, 21) (6, 25)
+
 ## Generate the Grid 1 km^2
     netgenerate -g --grid.number=5 --grid.length=250 --default.lanenumber=1 --default.speed 85 -o vehDist.net.xml
         # netgenerate -g --grid.number=5 --grid.length=250 --default.lanenumber=1 --no-turnarounds --default.speed 85 -o vehDist.net.xml
@@ -11,7 +13,7 @@
 ## Generate the Grid 3 km^2
     netgenerate -g --grid.number=13 --grid.length=250 --default.lanenumber=1 --default.speed 85 -o vehDist.net.xml
     ## Afer this comand run the script
-    ./grid_3km_convert.sh
+    ./gridConvertOneCharacter.sh 13
 
 ## Generate the trips and the routes (with various distance)
     python /media/sda4/prog/sumo-0.25.0/tools/randomTrips.py -n vehDist.net.xml --min-distance=100000 -b 0 -e 30000 -i 200 -s 1 -r vehDist_tmp.rou.xml
