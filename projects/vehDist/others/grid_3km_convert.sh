9e11d8d571ed709e5cfcc8012983531cbd926d29
João Batista <joao42lbatista@gmail.com>
2016-05-31 15:27:01 -0300

    Changed grid convert until 35 of count grid

--- projects/vehDist/others/grid_3km_convert.sh
+++ projects/vehDist/others/grid_3km_convert.sh
@@ -23,25 +23,62 @@
 # Script: Convert the values from a grid with 3 km (12 edges 0 to 12)
 # to use just one character in the edges
 #
-# Última atualização: 24/05/2016
+# Última atualização: 31/05/2016
 
-# 10 to a
-sed -i 's/to10/toa/g' vehDist.net.xml
-sed -i 's/_10/_a/g' vehDist.net.xml
-sed -i 's/\/10/\/a/g' vehDist.net.xml
-sed -i 's/10\//a\//g' vehDist.net.xml
+countGrid=$1
+if [ "$countGrid" == "" ]; then
+    echo "Error, need to pass the grid count to convert (e.g., $0 13)"
+    exit 1
+elif [ $countGrid -lt 10 ]; then
+    echo "Error, count grid need more than 9, (e.g., $0 13)"
+    exit 1
+elif [ $countGrid -gt 35 ]; then
+    echo "Error, can't work with count grid more than 35"
+    exit 1
+fi
 
-# 11 to b
-sed -i 's/to11/tob/g' vehDist.net.xml
-sed -i 's/_11/_b/g' vehDist.net.xml
-sed -i 's/\/11/\/b/g' vehDist.net.xml
-sed -i 's/11\//b\//g' vehDist.net.xml
+countGridTmp=10
+((countGrid++))
+echo -e "\nConverting...\n"
+while [ $countGridTmp -lt $countGrid ]; do
+    case $countGridTmp in
+        10) inValue=10; outValue="a" ;;
+        11) inValue=11; outValue="b" ;;
+        12) inValue=12; outValue="c" ;;
+        13) inValue=13; outValue="d" ;;
+        14) inValue=14; outValue="e" ;;
+        15) inValue=15; outValue="f" ;;
+        16) inValue=16; outValue="g" ;;
+        17) inValue=17; outValue="h" ;;
+        18) inValue=18; outValue="i" ;;
+        19) inValue=19; outValue="j" ;;
+        20) inValue=20; outValue="k" ;;
+        21) inValue=21; outValue="l" ;;
+        22) inValue=22; outValue="m" ;;
+        23) inValue=23; outValue="n" ;;
+        24) inValue=24; outValue="o" ;;
+        25) inValue=25; outValue="p" ;;
+        26) inValue=26; outValue="q" ;;
+        27) inValue=27; outValue="r" ;;
+        28) inValue=28; outValue="s" ;;
+        29) inValue=29; outValue="t" ;;
+        30) inValue=30; outValue="u" ;;
+        31) inValue=31; outValue="v" ;;
+        32) inValue=32; outValue="x" ;;
+        33) inValue=33; outValue="w" ;;
+        34) inValue=34; outValue="y" ;;
+        35) inValue=35; outValue="z" ;;
+    esac
 
-# 12 to c
-sed -i 's/to12/toc/g' vehDist.net.xml
-sed -i 's/_12/_c/g' vehDist.net.xml
-sed -i 's/\/12/\/c/g' vehDist.net.xml
-sed -i 's/12\//c\//g' vehDist.net.xml
+    sed -i 's/to'$inValue'/to'$outValue'/g' vehDist.net.xml
+    sed -i 's/_'$inValue'/_'$outValue'/g' vehDist.net.xml
+    sed -i 's/\/'$inValue'/\/'$outValue'/g' vehDist.net.xml
+    sed -i 's/'$inValue'\//'$outValue'\//g' vehDist.net.xml
+
+    ((countGridTmp++))
+    echo -e "\tTurning \"$inValue\" in \"$outValue\""
+done
+echo -e "\nConverting finished\n"
 
 exit 0
 #
