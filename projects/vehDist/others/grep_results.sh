156acc1dbf4ae52eee8cb0346d49780b818bd196
João Batista <joao42lbatista@gmail.com>
2016-06-02 10:01:31 -0300

    Renamed variables on grep_results.sh

--- projects/vehDist/others/grep_results.sh
+++ projects/vehDist/others/grep_results.sh
@@ -30,40 +30,39 @@ if [ "$part" == '' ]; then
    exit 1
 fi
 
-num_exp_i=$2
-if [ "$num_exp_i" == '' ]; then
-    num_exp_i=1
+numExpI=$2
+if [ "$numExpI" == '' ]; then
+    numExpI=1
 fi
 
-num_exp_f=$3
-if [ "$num_exp_f" == '' ]; then
-    num_exp_f=8
+numExpF=$3
+if [ "$numExpF" == '' ]; then
+    numExpF=8
 fi
 
-if [ "$num_exp_f" -lt "$num_exp_i" ]; then
-    num_exp_f=$num_exp_i
+if [ "$numExpF" -lt "$numExpI" ]; then
+    numExpF=$numExpI
 fi
 
-echo -e "\nNumber of experiments: $num_exp_i to $num_exp_f\n"
-((num_exp_f++))
+echo -e "\nNumber of experiments: $numExpI to $numExpF\n"
+((numExpF++))
 
-## Local pc folder
+## Local my pc folder
 cd /media/sda4/prog/simulation_veins/projects/vehDist/others/
 # or
-## Cluser folder, change de v001 for the veins version
+## Cluster folder, change de v001 for the veins version
 #cd /mnt/nfs/home/luz.marina/0_jonh/veins_v00$part/projects/vehDist$part/others/
 
 echo -e "\n## Script to collect the simulation results in one place\n"
 echo -e "\n   ## Results from vehDist $part\n"
 
 pathFolder="../results/vehDist_resultsEnd_$part"
-rsu_0_File="rsu\[0\]_Count_Messages_Received.r"
-vehs_file="Veh_Messages_Drop.r"
+rsu0File="rsu\[0\]_Count_Messages_Received.r"
+vehiclesFile="Veh_Messages_Drop.r"
 
 count=1
-continue_flag=1
-
-while [ $continue_flag == 1 ]; do
+continueFlag=1
+while [ $continueFlag == 1 ]; do
     case $count in
         1) experiment="0001_chosenByDistance" ;;
         2) experiment="0012_chosenByDistance_Speed" ;;
@@ -74,18 +73,18 @@ while [ $continue_flag == 1 ]; do
     esac
 
     echo -e "\n## Values from experiment $experiment\n"
-    i=$num_exp_i
-    while [ $i -lt $num_exp_f ]; do
+    i=$numExpI
+    while [ $i -lt $numExpF ]; do
         echo -e "\n               ## Experiment $i\n"
-        cat $pathFolder/$experiment/E$i_*/$rsu_0_File | grep -E "Exp: $i"
+        cat $pathFolder/$experiment/E$i_*/$rsu0File | grep -E "Exp: $i"
         echo
-        cat $pathFolder/$experiment/E$i_*/$vehs_file | grep -E "Exp: $i"
+        cat $pathFolder/$experiment/E$i_*/$vehiclesFile | grep -E "Exp: $i"
         ((i++))
     done
 
     ((count++))
     if [ $count == 7 ]; then
-        continue_flag=0
+        continueFlag=0
     fi
 done
 
