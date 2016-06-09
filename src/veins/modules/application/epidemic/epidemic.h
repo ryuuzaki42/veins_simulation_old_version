8c41dc1ae95aec3728e826f7bed704aef71826de
João Batista <joao42lbatista@gmail.com>
2016-06-09 15:41:57 -0300

    Organized and cleaned 2

--- src/veins/modules/application/epidemic/epidemic.h
+++ src/veins/modules/application/epidemic/epidemic.h
@@ -1,22 +1,4 @@
-//
-// Copyright (C) 2006-2011 Christoph Sommer <christoph.sommer@uibk.ac.at>
-//
-// Documentation for these modules is at http://veins.car2x.org/
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//
+// Copyright (C) 2015-2016 João Batista <joao.b@usp.br>
 
 #ifndef epidemic_H
 #define epidemic_H
@@ -38,18 +20,15 @@ class epidemic : public BaseWaveApplLayer {
         TraCICommandInterface* traci;
         TraCICommandInterface::Vehicle* traciVehicle;
 
-        static unsigned short int messageId;
-
     protected:
         void onBeacon(WaveShortMessage* wsm);
         void onData(WaveShortMessage* wsm);
 
-        void generateTarget(); // generate a target to send a message
-        void generateMessage(); // generate a message to send a message
-
         void finish();
-};
 
-unsigned short int epidemic::messageId;
+        void sendWSM(WaveShortMessage* wsm);
+
+        void vehInitializeVariablesEpidemicVeh();
+};
 
 #endif
